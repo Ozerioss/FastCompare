7af75081b2c45b5d606a249b1d2684df05157d7e
Karim Kidiss <kkidiss@yahoo.fr>
2017-01-25 15:20:41 +0100

    added a function to fetch the url typed by the user

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const http = require('http');
+/*const http = require('http');
+
+const url = require('url');
 
 const fs = require('fs');
 const hostname = '127.0.0.1'
@@ -19,8 +21,57 @@ fs.readFile('index.html', (err,html) =>
 	res.write(html);
 	});
 
+	var lien = document.getElementbyId('URL').innerText;
+
+	console.log(lien);
 	server.listen(port, hostname, () => {
 	console.log('Server started on port', port);
 	});
+});*/
+
+var http = require('http');
+var fs = require('fs');
+var formidable = require("formidable");
+var util = require('util');
+
+var server = http.createServer(function (req, res) {
+	if(req.method.toLowerCase()=='get')
+	{
+		displayForm(res);
+	}
+	else if (req.method.toLowerCase()=='post') 
+	{
+		processAllFieldsOfTheForm(req,res);
+	}
 });
 
+function displayForm(res) {
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+                'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
+
+function processAllFieldsOfTheForm(req,res)
+{
+	var form = new formidable.IncomingForm();
+
+	form.parse(req, function(err,fields,files)
+	{
+		res.writeHead(200, {
+			'content-type': 'text/plain'
+		});
+		res.write('received the data : \n \n');
+		res.end(util.inspect(
+		{
+			fields : fields,
+			files : files
+		}));
+	});
+}
+server.listen(3000);
+console.log("server listening on 1185");
